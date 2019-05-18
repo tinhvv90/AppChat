9e491c517eaf973e952a631f470a8e793a5cacee
TinhVV <tinhvv90@gmail.com>
2019-05-18 08:02:31 +0700

    init project

---
+++ AppChat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AppChat
+//
+//  Created by CenHomes on 5/18/19.
+//  Copyright © 2019 Simson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
